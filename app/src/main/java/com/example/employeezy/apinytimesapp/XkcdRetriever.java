39495e7496f1f56bd5abf0c26348197b12017522
Michael Marshall <hmikemarshall@gmail.com>
2016-05-20 15:27:49 -0700

    implements loopj

--- app/src/main/java/com/example/employeezy/apinytimesapp/XkcdRetriever.java
+++ app/src/main/java/com/example/employeezy/apinytimesapp/XkcdRetriever.java
@@ -1,5 +1,59 @@
 package com.example.employeezy.apinytimesapp;
 
+import android.widget.EditText;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
 /**
  * Created by EmployYeezy on 5/20/16.
  */
+
+public class XkcdRetriever {
+
+   EditText searchValue;
+    String idNumber;
+
+    private static XkcdRetriever intance;
+    private static ApiResponseHandler responseHandler;
+
+    private XkcdRetriever() {
+    }
+
+    public static XkcdRetriever getIntance(ApiResponseHandler handler) {
+        responseHandler = handler;
+        if (intance == null) {
+            intance = new XkcdRetriever();
+        }
+        return intance;
+    }
+
+    public void doRequest() {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        String foo;
+        client.get(
+                "http://xkcd.com/234/info.0.json",
+                null,
+                new JsonHttpResponseHandler() {
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+
+                        try {
+                            responseHandler.handleResponse(response.getString("month"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+    }
+
+    public interface ApiResponseHandler {
+        void handleResponse(String response);
+    }
+}
